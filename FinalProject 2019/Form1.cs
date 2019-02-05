c580b6350e84efdc2e627c87f951398ef49b8aed
agnoam <agnoam.aghai.ooo.123@gmail.com>
2019-02-05 18:24:32 +0200

    styling the admin's form

--- FinalProject 2019/Form1.cs
+++ FinalProject 2019/Form1.cs
@@ -4,14 +4,22 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Windows.Forms;
 
 namespace FinalProject_2019 {
-    public partial class Form1 : Form {
-        public Form1()
-        {
+    public partial class LoginPage : Form {
+        public LoginPage() {
             InitializeComponent();
+        }
+
+        private void UsernameTextBox_TextChanged(object sender, EventArgs e) {
+
+        }
+
+        private void closeForm_Click(object sender, EventArgs e) {
+            Application.Exit();
         }
     }
 }
